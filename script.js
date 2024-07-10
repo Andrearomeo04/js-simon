de6384dab55ed7bb9d9dd5fbcd7f87b553f72efb
andrearomeo04 <pisco004@gmail.com>
2024-07-10 05:05:53 +0200

    [FIX] fixed all problems that showed errors

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generate_random_numbers (num_arr, max, min){
 const random = generate_random_numbers (5, 100, 1);
 
 function start () {
-    random.innerHTML = ''
+    show_num.innerHTML = ''
 
     let user_numbers = []
 
@@ -26,33 +26,36 @@ function start () {
 
     const compared_numbers = []
         
-    for (let i = 0; i < compared_numbers.length; i++) {
-        let final_numbers = casual_numbers[i]
+    for (let i = 0; i < random.length; i++) {
+        let final_numbers = random[i]
         if (user_numbers.includes(final_numbers)) {
             compared_numbers.push(final_numbers)
         }
     }
 
+    let result = document.getElementById('final_message');
+
+
+
     if (compared_numbers.length === 1) {
-        result.innerHTML = `Al pelo! Hai indovinato ${compared_numbers.length} numeri! (${compared_numbers}) `
+        result.innerHTML = `Al pelo! Hai indovinato ${compared_numbers.length} numeri u 5! (${compared_numbers}) `
+    } else if (compared_numbers.length === 2 || compared_numbers.length === 3 ) {
+        result.innerHTML = `Poteva andare meglio... Hai indovinato ${compared_numbers.length} numeri su 5! (${compared_numbers}) `
+    } else if (compared_numbers.length === 4) {
+        result.innerHTML = `Ci sei andato vicinissimo! Hai indovinato ${compared_numbers.length} numeri su 5! (${compared_numbers}) `
+    } else if (compared_numbers.length === 5) {
+        result.innerHTML = `Complimenti! Ma come hai fatto? Hai indovinato ${compared_numbers.length} numeri su 5! (${compared_numbers}) `
     }
-    if (compared_numbers.length === 2 || compared_numbers.length === 3 ) {
-        result.innerHTML = `Poteva andare meglio... Hai indovinato ${compared_numbers.length} numeri! (${compared_numbers}) `
-    }
-    if (compared_numbers.length === 4) {
-        result.innerHTML = `Ci sei andato vicinissimo! Hai indovinato ${compared_numbers.length} numeri! (${compared_numbers}) `
-    }
-    if (compared_numbers.length === 5) {
-        result.innerHTML = `Complimenti! Ma come hai fatto? Hai indovinato ${compared_numbers.length} numeri! (${compared_numbers}) `
-    }
+
+
         
 }
 
-
-
 let show_num = document.getElementById('numbers');
-let result = document.getElementById('final_message');
 
 show_num.innerHTML = random.join(' ')
+
+
+setTimeout(start, 5000)
 
 
